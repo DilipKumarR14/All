0f7449ecf0095b24075d948c5ee4abc5c739fd0a
DilipVijay <42669655+DilipVijay@users.noreply.github.com>
2018-10-08 19:30:33 +0530

    login route

--- codeigniter/application/controllers/Account.php
+++ codeigniter/application/controllers/Account.php
@@ -1,7 +1,7 @@
 <?php
 // if not set we will not get back the response back to front-end(rest calls)
 header('Access-Control-Allow-Origin: *');
-include_once("Config.php");
+include_once "Config.php";
 class Account
 {
     public function register()
@@ -18,17 +18,16 @@ class Account
         $password = $_POST['password'];
 
         // Create connection
-        $flag=1;
-        $conf= new Config();
-        $conn=$conf->configs();
-        $dupli=mysqli_query($conn,"select mobile,email from users where email='$email' or mobile='$mobile'  ");
-        if(mysqli_num_rows($dupli) > 0){
+        $conf = new Config();
+        $conn = $conf->configs();
+        $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$mobile'  ");
+        if (mysqli_num_rows($dupli) > 0) {
             // $data = array("duplicate\n");
             // print (json_encode("Duplicate Entry "));
-            print ("Duplicate Record");
-            
-          }else {
-           
+            print("Duplicate Record");
+
+        } else {
+
             $sql = "INSERT INTO users (name, email, mobile, password)
             VALUES ('$name','$email','$mobile','$password')";
             if ($conn->query($sql) === true) {
@@ -36,9 +35,32 @@ class Account
             } else {
                 echo "Error: " . $sql . "<br>" . $conn->error;
             }
-            $dupli=mysqli_query($conn,"select mobile,email from users where email='$email' or mobile='$mobile'  ");
-            $myjson='{"name":'.'"'.$name.'",".email":'.'"'.$email.'","mobile":'.$mobile."}"; 
+            $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$mobile'  ");
+            $myjson = '{"name":' . '"' . $name . '",".email":' . '"' . $email . '","mobile":' . $mobile . "}";
             print $myjson;
+        }
+    }
+    public function logins()
+    {
+        /**
+         * @var email holds the email from form
+         * @var passwd holds the password from form
+         */
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $conf = new Config();
+        $conn = $conf->configs();
+        $dupli = mysqli_query($conn, "select password,email from users where email='$email' or mobile='$password'  ");
+        if (mysqli_num_rows($dupli) > 0) {
+
+            print("Registered User");
+
+        } else {
+
+            print ("User Is Not Registered");
+            // $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$mobile'  ");
+            // $myjson = '{"name":' . '"' . $name . '",".email":' . '"' . $email . '","mobile":' . $mobile . "}";
+            // print $myjson;
         }
     }
 }
