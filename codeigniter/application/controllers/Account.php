85a1b60ea29ae8d95356dff7fb777796af9863e1
DilipVijay <42669655+DilipVijay@users.noreply.github.com>
2018-10-09 18:14:21 +0530

    Add files via upload

--- codeigniter/application/controllers/Account.php
+++ codeigniter/application/controllers/Account.php
@@ -10,35 +10,66 @@ class Account
          * @var name holds the name from form
          * @var email holds the email from form
          * @var mobile holds the mobile from form
-         * @var passwd holds the password from form
+         * @var password holds the password from form
          */
+
         $name = $_POST['name'];
         $email = $_POST['email'];
         $mobile = $_POST['mobile'];
         $password = $_POST['password'];
-
-        // Create connection
         $conf = new Config();
         $conn = $conf->configs();
-        $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$mobile'  ");
-        if (mysqli_num_rows($dupli) > 0) {
-            // $data = array("duplicate\n");
-            // print (json_encode("Duplicate Entry "));
-            print("Duplicate Record");
+        // $res=$conn->prepare("SELECT email where email='$email'");
+        $sqlmail="select email from users where email='$email'";
+        $sqlmobile="select mobile from users where mobile='$mobile'";
+        // return true is value is present else false(query/fetch)
+        $qm=$conn->query($sqlmobile)->fetch();
+        $qe=$conn->query($sqlmail)->fetch();
 
-        } else {
+        // Create connection
+        if($qm == true && $qe == true){
+            echo "Already EXist\n";
 
-            $sql = "INSERT INTO users (name, email, mobile, password)
-            VALUES ('$name','$email','$mobile','$password')";
-            if ($conn->query($sql) === true) {
-                //echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-            $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$mobile'  ");
-            $myjson = '{"name":' . '"' . $name . '",".email":' . '"' . $email . '","mobile":' . $mobile . "}";
-            print $myjson;
         }
+        else{
+        try {
+            //echo "Success";
+            $conf = new Config();
+            $conn = $conf->configs();
+            
+            $stm = $conn->prepare("INSERT INTO users(name,email,mobile,password) VALUES('$name','$email','$mobile','$password')");
+
+            $stm->execute(
+                array(
+                    ':name' => '$name',
+                    ':email' => '$email',
+                    ':mobile' => '$mobile',
+                    ':password' => '$password')
+                );
+            $myjson = '{"name":' . '"' . $name . '","email":' . '"' . $email . '","mobile":' . $mobile . "}";
+           print $myjson;
+        } catch (PDOException $e) {
+            echo "NOt SAved" . $e->getMessage();
+        }
+    }
+        // if (mysqli_num_rows($dupli) > 0) {
+        //     // $data = array("duplicate\n");
+        //     // print (json_encode("Duplicate Entry "));
+        //     print("Duplicate Record");
+
+        // } else {
+        //     echo "hello world";
+        //     $sql = "INSERT INTO users (name, email, mobile, password)
+        //     VALUES ('$name','$email','$mobile','$password')";
+        //     if ($conn->exec($sql) === true) {
+        //         echo "New record created successfully";
+        //     } else {
+        //         echo "Error: " . $sql . "<br>" . $conn->error;
+        //     }
+        //     $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$mobile'  ");
+        //     $myjson = '{"name":' . '"' . $name . '",".email":' . '"' . $email . '","mobile":' . $mobile . "}";
+        //     print $myjson;
+        // }
     }
     public function logins()
     {
@@ -50,17 +81,17 @@ class Account
         $password = $_POST['password'];
         $conf = new Config();
         $conn = $conf->configs();
+        echo "Hello world\n";
         $dupli = mysqli_query($conn, "select password,email from users where email='$email' or mobile='$password'  ");
         if (mysqli_num_rows($dupli) > 0) {
 
-            print("Registered User");
+            print("Registered User\n");
+            $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$password'  ");
+            $myjson = '{"email":' . '"' . $email . '","password":' . '"' . $password . '"}';
+            print $myjson;
 
         } else {
-
-            print ("User Is Not Registered");
-            // $dupli = mysqli_query($conn, "select mobile,email from users where email='$email' or mobile='$mobile'  ");
-            // $myjson = '{"name":' . '"' . $name . '",".email":' . '"' . $email . '","mobile":' . $mobile . "}";
-            // print $myjson;
+            print("User Is Not Registered\n");
         }
     }
 }
--- codeigniter/application/controllers/Config.php
+++ codeigniter/application/controllers/Config.php
@@ -5,22 +5,29 @@
  *******************************/
 class Config
 {
-	/**
-	 * @method Establish the Connection 
-	 * @var conn Establish the Connection with DataBase
-	 * @return Object
-	 */
-	function configs()
-	{
-// Create connection
-		$conn = new mysqli("localhost","root","admin","account");
 
-// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}else{
-			return $conn;
-		}
-	}
+    /**
+     * @method Establish the Connection
+     * @var conn Establish the Connection with DataBase
+     * @return Object
+     */
+    public function configs()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "admin";
+        $dbname = "account";
+// Create connection
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=account", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //  echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+	return $conn;
+    }
 }
-?>
